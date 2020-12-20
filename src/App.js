20ed6381417726adf6889534a3aa5269e84e2f3e
Richard Quigley <richard@rqwebdev.com>
2020-12-20 23:11:42 +0000

    added fonts and default styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import GlobalStyles from "./components/GlobalStyles";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
