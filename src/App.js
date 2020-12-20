8485476c96ba9e91024c73c64df4110a94d9ce9e
Richard Quigley <richard@rqwebdev.com>
2020-12-20 23:27:03 +0000

    Added logo and fonts

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <h1>Hello</h1>
+      <h2>FilmTaster</h2>
     </div>
   );
 }
--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -23,7 +23,7 @@ const GlobalStyles = createGlobalStyle`
     }
     h2 {
         font-size: 3rem;
-        font-family: 'Abril Fatface', cursive;
+        font-family: 'Poiret One', cursive;
         font-weight: lighter;
         color: #333;
     }
